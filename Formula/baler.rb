08ad19de8a23458afaa5698f90900a4891a272d1
Fritjof <fritjof.folkeson@gmail.com>
2023-06-26 15:37:57 +0200

    update small format

--- Formula/baler.rb
+++ Formula/baler.rb
@@ -1,12 +1,12 @@
 class Baler < Formula
   include Language::Python::Virtualenv
 
-  desc "Baler is a machine learning based compression tool for scientific data."
+  desc "A machine learning based compression tool for scientific data"
   homepage "https://baler-collaboration.github.io/"
   url "https://github.com/fritjof-b/baler/archive/refs/tags/brew-experimental.tar.gz"
   version "1.0"
   sha256 "23e15a70d58a8625d7b3e9a074d88a96bfe2b0054e22a17bbcffb3744b749d1b"
-  license "Apache 2.0 License"
+  license "Apache-2.0"
 
   depends_on "poetry" => :build
   depends_on "python@3.9"
