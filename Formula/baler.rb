7d94b54f9524acf53a88265531e3c2c9ae744385
Fritjof <fritjof.folkeson@gmail.com>
2023-06-27 10:45:20 +0200

    working formula

--- Formula/baler.rb
+++ Formula/baler.rb
@@ -4,17 +4,19 @@ class Baler < Formula
 
   desc "Machine learning based compression tool for scientific data"
   homepage "https://baler-collaboration.github.io/"
-  url "https://github.com/fritjof-b/baler/archive/refs/tags/brew-experimental.tar.gz"
+  url "https://github.com/fritjof-b/baler/archive/refs/tags/v1.tar.gz"
   version "1.0"
-  sha256 "23e15a70d58a8625d7b3e9a074d88a96bfe2b0054e22a17bbcffb3744b749d1b"
+  sha256 "06a6b577e3f8ac48815993af9434e98ed36b4ba7994c2f9a913d6d0c487c6e0d"
   license "Apache-2.0"
 
-#   depends_on "poetry" => :build
   depends_on "python@3.9"
+    
 
   def install
-    rewrite_shebang detected_python_shebang, "baler"
-    bin.install "baler"
+    virtualenv_create(libexec, "python3.9")
+    system libexec/"bin/pip", "install", "-r", buildpath/"requirements.txt"
+    system libexec/"bin/pip", "install", "-v", "--no-deps", "--no-binary", ":all", buildpath
+    bin.install_symlink libexec/"bin/baler"
   end
 
   test do
