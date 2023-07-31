bffd46f5e8fea4c5d5dc1e3a7493e9046e03d4ad
Fritjof Bengtsson <fritjof.folkeson@gmail.com>
2023-07-31 12:51:43 +0200

    remove redundant version spec

--- Formula/baler.rb
+++ Formula/baler.rb
@@ -5,7 +5,6 @@ class Baler < Formula
   desc "Machine learning based compression tool for scientific data"
   homepage "https://baler-collaboration.github.io/"
   url "https://github.com/fritjof-b/baler/archive/refs/tags/v1.4.0.tar.gz"
-  version "1.4.0"
   sha256 "37e4522d00b35d9e31b0eb5995a8a8d5ddbbedcfaa7c9628ceaadd262641a2df"
   license "Apache-2.0"
 
